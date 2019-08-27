a7792b679da9bc80dc88d4810a5c42803113a825
Amornpan Phornchaicharoen <amornpan@gmail.com>
2019-08-27 14:00:14 +0700

    Google map start

--- lib/screens/show_map.dart
+++ lib/screens/show_map.dart
@@ -1,4 +1,6 @@
+import 'package:first_flutter/main.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowMap extends StatefulWidget {
   @override
@@ -6,11 +8,24 @@ class ShowMap extends StatefulWidget {
 }
 
 class _ShowMapState extends State<ShowMap> {
+  //Explicit
+  static const totinnovate_LatLng = const LatLng(14.019996, 100.527670);
+  CameraPosition cameraPosition = CameraPosition(
+    target: totinnovate_LatLng,
+    zoom: 16.0,
+  );
+
+  //Method
+  myShowMap() {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: cameraPosition,
+      onMapCreated: (GoogleMapController googleMapController) {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Show Map'),);
+    return myShowMap();
   }
 }
-
-
-
